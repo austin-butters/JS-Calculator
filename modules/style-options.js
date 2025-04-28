aba2252ec821b598ca3f65dce1b68812828c8cf8
Austin Butters <buttersaustin2020@gmail.com>
2025-04-29 03:32:09 +1200

    Add default application order for styles

--- modules/style-options.js
+++ modules/style-options.js
@@ -4,7 +4,40 @@
 // Must apply in a specific order, usually from most general to most specific (e.g. all buttons, then specific button colors).
 // In the object, keys are the variable name, and values are the values to assign.
 
-const defaultApplicationOrder = ['--body-background-color']
+// The current variables I'm using are just for color. They are (as of 29/04/2025, 03:29AM)
+// --body-background-color
+// --calculator-container-background-color
+// --calculator-display-background-color
+// --calculator-display-border-color
+// --displayed-text-font-family
+// --displayed-text-font-size
+// --displayed-text-color
+// --button-background-color
+// --button-font-size
+// --buttontype-value-background-color
+// --buttontype-setting-background-color
+// --buttontype-operator-background-color
+// --buttontype-basic-operator-background-color
+// --buttontype-constant-background-color
+// --buttontype-integer-background-color
+
+const defaultApplicationOrder = [
+  '--body-background-color',
+  '--calculator-container-background-color',
+  '--calculator-display-background-color',
+  '--calculator-display-border-color',
+  '--displayed-text-font-family',
+  '--displayed-text-font-size',
+  '--displayed-text-color',
+  '--button-background-color',
+  '--button-font-size',
+  '--buttontype-value-background-color',
+  '--buttontype-setting-background-color',
+  '--buttontype-operator-background-color',
+  '--buttontype-basic-operator-background-color',
+  '--buttontype-constant-background-color',
+  '--buttontype-integer-background-color',
+]
 
 export const style0 = {
   applicationOrder: [defaultApplicationOrder],
